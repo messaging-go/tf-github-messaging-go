42075b6259b376cdf88431b42e21886979619b70
Nishchal Gautam <gautam.nishchal@gmail.com>
2026-09-10 21:05:24 +0400

    fix: specify removal

--- repo_pubsub.tf
+++ repo_pubsub.tf
@@ -4,3 +4,9 @@ module "repository_pubsub" {
   description = "A compatiable integration for version 2 of google pubsub"
   topics      = ["google", "pubsub", "gcloud", "gcp"]
 }
+removed {
+  from = module.repository_pubsub
+  lifecycle {
+    destroy = true
+  }
+}
