69d76515194ab29c2b7a3195238a260dc1f4611d
Nishchal Gautam <cyberhck@users.noreply.github.com>
2026-09-10 09:40:22 +0400

    feat: add template repo (#5)

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    hostname = "otaco.app"
+    hostname     = "otaco.app"
     organization = "org_01M24P27F6715RHDPKSSCT1A1W"
     workspaces {
-      name = "dfb837b2-2ad1-41e0-a968-bbf5b7794dc0"
+      name = "79cfb695-2517-429b-b2d3-8fabf66cbdde"
     }
   }
   required_providers {
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "github" {
-  owner = "messaging-go"
+  owner             = "messaging-go"
   parallel_requests = true
+  token             = var.gh_token
 }
-
