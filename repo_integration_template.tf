3a7b4db50ab37159884884518f27a1e04a0d8f5f
Nishchal Gautam <gautam.nishchal@gmail.com>
2026-09-10 08:59:21 +0400

    feat: create integration template repo

--- repo_integration_template.tf
+++ repo_integration_template.tf
@@ -1,7 +1,7 @@
 module "repository_integration_template" {
   source                 = "./modules/repository"
   description            = "A GitHub template repository to start integrations off from. Use this template when you want to create middlewares for adding support for messaging platform, adding utilities, etc."
-  topics                 = ["template"]
+  topics                 = ["template", "integration"]
   name                   = "integration-template"
   required_status_checks = ["test", "release", "lint"]
   is_template            = true
