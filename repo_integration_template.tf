157cda04f1896078ee18f02c8189c4d24a305246
Nishchal Gautam <gautam.nishchal@gmail.com>
2026-09-10 08:43:11 +0400

    feat: add initial repository

---
+++ repo_integration_template.tf
@@ -0,0 +1,8 @@
+module "repository_integration_template" {
+  source                 = "./modules/repository"
+  description            = "A GitHub template repository to start integrations off from. Use this template when you want to create middlewares for adding support for messaging platform, adding utilities, etc."
+  topics                 = ["template"]
+  name                   = "integration-template"
+  required_status_checks = ["test", "release", "lint"]
+  is_template            = true
+}
