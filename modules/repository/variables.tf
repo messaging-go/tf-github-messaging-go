14bb8205fa12ef05bad9c2df8ff8f01be3371478
Nishchal Gautam <gautam.nishchal@gmail.com>
2026-09-10 08:43:11 +0400

    feat: add initial repository

---
+++ modules/repository/variables.tf
@@ -0,0 +1,40 @@
+variable "name" {
+  type        = string
+  description = "The name of the github repository"
+  sensitive   = false
+}
+
+variable "description" {
+  type        = string
+  description = "The description of the repository"
+  sensitive   = false
+}
+
+variable "topics" {
+  type        = list(string)
+  description = "The topics to attach to the repository"
+  sensitive   = false
+}
+
+
+variable "homepage_url" {
+  type        = string
+  description = "The homepage URL to set"
+  default     = null
+  nullable    = true
+  sensitive   = false
+}
+
+variable "is_template" {
+  type        = bool
+  description = "Create the repository as a template repository"
+  default     = false
+  sensitive   = false
+}
+
+variable "required_status_checks" {
+  type        = list(string)
+  description = "The status checks to enforce to merge on to the default branch"
+  default     = []
+  sensitive   = false
+}
