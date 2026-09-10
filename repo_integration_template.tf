54fa3bf202afd838820ab60ce57bcbbb1b174258
Nishchal Gautam <cyberhck@users.noreply.github.com>
2026-09-10 19:21:59 +0400

    feat: use correct status check names (#6)

--- repo_integration_template.tf
+++ repo_integration_template.tf
@@ -3,6 +3,6 @@ module "repository_integration_template" {
   description            = "A GitHub template repository to start integrations off from. Use this template when you want to create middlewares for adding support for messaging platform, adding utilities, etc."
   topics                 = ["template", "integration"]
   name                   = "integration-template"
-  required_status_checks = ["test", "release", "lint"]
+  required_status_checks = ["Test", "Semantic Release", "Lint"]
   is_template            = true
 }
